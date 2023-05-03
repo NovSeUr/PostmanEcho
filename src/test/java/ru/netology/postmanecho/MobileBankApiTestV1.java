80addd83d5d0bd9caa21024ab9ac312da9478a91
Nov.s <novikov.su20@gmail.com>
2023-05-04 01:22:29 +0500

    change for success test

--- src/test/java/ru/netology/postmanecho/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/postmanecho/MobileBankApiTestV1.java
@@ -20,7 +20,7 @@ class MobileBankApiTestV1 {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some vaue"))
+                .body("data", equalTo("some value"))
         ;
     }
 }
